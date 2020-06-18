b9a21ec57ef514010c1dd7ef787e48a5ff961fa4
Fuat Ulugay <fuat.ulugay@gmail.com>
2020-06-18 19:54:33 +0300

    added extension check for file upload

--- fileupload/models.py
+++ fileupload/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+
+def validate_file_extension(value):
+    import os
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
+    valid_extensions = ['.jpg', '.png', '.gif']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError('Unsupported file extension.')
+
 # Create your models here.
 class File(models.Model):
-    file = models.FileField(blank=False, null=False)
+    file = models.FileField(blank=False, null=False, validators=[validate_file_extension])
     def __str__(self):
         return self.file.name
